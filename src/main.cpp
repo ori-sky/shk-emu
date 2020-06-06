438092dc4090a3324fc0efc55cd93825e0671f41
David Farrell <shokku.ra@gmail.com>
2020-06-06 17:50:46 +0100

    Add basic decode/execute loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,76 @@
 #include <unistd.h>
 
 #include <fstream>
+#include <iomanip>
 #include <vector>
 
 #include <shk.h>
 
-shk::instruction decode_one(std::istream &is) {
+struct cpu_state {
+	uint16_t mem[0x10000];
+	uint16_t reg[0x100];
+	uint16_t ip;
+};
+
+void debug(cpu_state &state) {
+	for(size_t i = 0x0; i < 0x8; ++i) {
+		std::cout << "mem[";
+		std::cout << "0x" << std::setfill('0') << std::setw(4) << std::hex << i;
+		std::cout << "] = ";
+		std::cout << "0x" << std::setfill('0') << std::setw(4) << std::hex << state.mem[i];
+		std::cout << std::endl;
+	}
+}
+
+std::optional<shk::instruction> decode(cpu_state &state) {
 	shk::instruction instr;
 
-	uint8_t hi, lo;
-	is.read(reinterpret_cast<char *>(&hi), 1);
-	is.read(reinterpret_cast<char *>(&lo), 1);
-	uint16_t byte = (uint16_t(hi) << 8u) | uint16_t(lo);
-
-	if(byte >> 15u) {
+	if(state.mem[state.ip] >> 15u) {
+		/*
 		shk::command command;
-		command.ty = static_cast<shk::command::type>(byte & 0xF);
+		command.ty = static_cast<shk::command::type>(state.mem[state.ip] & 0xF);
 
 		is.read(reinterpret_cast<char *>(&hi), 1);
 		is.read(reinterpret_cast<char *>(&lo), 1);
 
 		instr = decode_one(is);
 		instr.commands.emplace_back(command);
-	} else {
-		instr.op = static_cast<shk::opcode>(byte);
-
-		for(size_t i = 0; i < shk::num_operands(instr.op); ++i) {
-			is.read(reinterpret_cast<char *>(&hi), 1);
-			is.read(reinterpret_cast<char *>(&lo), 1);
-		}
+		*/
+		std::cerr << "commands not supported yet" << std::endl;
+		return {};
 	}
 
+	instr.op = static_cast<shk::opcode>(state.mem[state.ip++]);
+	for(size_t i = 0; i < shk::num_operands(instr.op); ++i) {
+		shk::operand operand;
+		operand.ty = shk::operand::type::reg; // XXX
+		operand.value = state.mem[state.ip++] & 0xFF;
+		instr.operands.emplace_back(operand);
+	}
+
+	std::cout << "decoded " << instr.op << std::endl;
 	return instr;
 }
 
-std::vector<shk::instruction> decode(std::istream &is) {
-	std::vector<shk::instruction> instrs;
-
-	while(is.peek() != EOF) {
-		instrs.emplace_back(decode_one(is));
+bool execute(cpu_state &state) {
+	if(auto instr = decode(state)) {
+		switch(instr->op) {
+		case shk::opcode::debug:
+			debug(state);
+			std::cout << "Hit enter to continue" << std::endl;
+			getc(stdin);
+			break;
+		case shk::opcode::store:
+			state.mem[instr->operands[0].value] = instr->operands[1].value;
+			break;
+		default:
+			break;
+		}
+		std::cout << "executed " << instr->op << std::endl;
+		return true;
+	} else {
+		return false;
 	}
-
-	return instrs;
 }
 
 int main(int argc, char *argv[]) {
@@ -59,17 +88,35 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 
+	cpu_state state;
+	state.ip = 0;
+
 	for(size_t i = 0; i < in_count; ++i) {
 		char *in_path = argv[i + optind];
 
 		std::ifstream is(in_path, std::ios::in | std::ios::binary);
 
+		while(is.peek() != EOF) {
+			uint8_t hi, lo;
+			is.read(reinterpret_cast<char *>(&hi), 1);
+			is.read(reinterpret_cast<char *>(&lo), 1);
+			uint16_t byte = (uint16_t(hi) << 8u) | uint16_t(lo);
+			state.mem[state.ip++] = byte;
+		}
+
+		/*
 		auto instrs = decode(is);
 
 		for(auto &instr : instrs) {
 			std::cout << instr.op << std::endl;
+			state.mem[state.ip++] = instr.op
 		}
+		*/
 	}
+
+	state.ip = 0;
+
+	while(execute(state)) {}
 
 	return 0;
 }
